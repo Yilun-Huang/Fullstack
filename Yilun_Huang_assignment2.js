18295c76e494e8ab66288b74fbc03ab43dcf941d
Yilun-Huang <83981298+yilunhuang88@users.noreply.github.com>
2022-01-05 16:24:08 -0700

    New file added

---
+++ Yilun_Huang_assignment2.js
@@ -0,0 +1,35 @@
+const itemObject = [
+  { quantity: 1, price: 200 },
+  { quantity: 3, price: 350 },
+  { quantity: 5, price: 400 },
+];
+
+/*Q1*/
+let newItemObject = itemObject.map((item) => {
+  item.quantity *= 2;
+  item.price *= 2;
+  return item;
+});
+console.log(newItemObject);
+
+/*Q2*/
+newItemObject = itemObject.filter(
+  (item) => item.quantity > 2 && item.price > 300
+);
+console.log(newItemObject);
+
+/*Q3*/
+const total = itemObject.reduce((acc, item) => {
+  return acc + item.quantity * item.price;
+}, 0);
+console.log(total);
+
+/*Q4*/
+const string =
+  " Perhaps The Easiest-to-understand   Case   For Reduce Is   To Return The Sum Of  All The Elements In  An Array  ";
+const newString = string
+  .trim()
+  .replace(/-/g, "")
+  .replace(/\s+/g, " ")
+  .toLowerCase();
+console.log(newString);
